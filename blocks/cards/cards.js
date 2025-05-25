dda431f21122802c6cb2b0601cdb258c0125d561
Tejasvi Jain <dinal.tejasvi@gmail.com>
2025-05-25 15:17:25 +0530

    added placeholders and redirects

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,12 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+import { fetchPlaceholders } from '../../scripts/aem.js';
+
+
+export default async function decorate(block) {
+
+  const placeholders = await fetchPlaceholders();
+  console.log(placeholders);
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -9,10 +15,22 @@ export default function decorate(block) {
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
+
     });
+    if (placeholders.clickHere) {
+      const placeholderElement = document.createElement('button');
+      placeholderElement.className = 'c';
+      placeholderElement.textContent = placeholders.clickHere;
+      li.append(placeholderElement);
+    }
     ul.append(li);
+
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
 }
+
+
+
+
